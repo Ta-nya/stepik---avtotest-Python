b5624dc3b20f57d5510b0ea5b65f25addb5a625d
Ta-nya <leeprae@gmail.com>
2020-07-07 23:25:03 +0300

    Небольшое изменение теста и фикстуры
    фикстура - увеличено время функции time.sleep() до 30 сек
    тест - удалены лишние данные

--- conftest.py
+++ conftest.py
@@ -10,13 +10,13 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("--language")
-    print(f"\nstart browser for test in {user_language}...")
+    print(f"\nstart browser for test in {user_language}")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     link = f"http://selenium1py.pythonanywhere.com/{user_language}/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,17 +1,7 @@
-
-
 def test_button_add_to_basket_is_displayed(browser):
     button = browser.find_element_by_css_selector(".btn.btn-add-to-basket")
-    assert button.is_displayed(), f"Button 'add_to_basket' is not displayed"
-    return print(button)
+    assert button.is_displayed(), "Button is not displayed"
 
 
-#
-# link = "https://www.google.com/"
-#
-# def test_button(browser):
-#     browser.get(link)
-#     input1 = browser.find_element_by_css_selector("input.gNO89b")
-#     print(f"text = \'{input1}\'")
 
 
