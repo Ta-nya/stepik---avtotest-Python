f14c4b26e289b4353a3a39c3c20ef2a8b26c7f8d
Ta-nya <leeprae@gmail.com>
2020-07-22 23:57:32 +0300

    Add new mark for tests

--- test_items.py
+++ test_items.py
@@ -1,7 +1,16 @@
 def test_button_add_to_basket_is_displayed(browser):
     button = browser.find_element_by_css_selector(".btn.btn-add-to-basket")
+
     assert button.is_displayed(), "Button is not displayed"
 
+    elements = browser.find_elements_by_css_selector(".btn.btn-add-to-basket")
+    assert len(elements) > 0, "Button is not displayed"
+
+    # element = None
+    # try:
+    #     element = browser.find_elements_by_css_selector(".btn.btn-add-to-basket")
+    # except NoSuchElementException:
+    #     print("Button is not displayed")
 
 
 
